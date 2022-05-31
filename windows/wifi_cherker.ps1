cdcdc65e1d7f565ab20234393607de20b3956586
filip2cz <filda.komarek@seznam.cz>
2022-05-31 18:51:35 +0200

    english support

--- windows/wifi_cherker.ps1
+++ windows/wifi_cherker.ps1
@@ -2,14 +2,13 @@
 while($test -lt 3)
 {
     if(Test-Connection 8.8.8.8 -Quiet){
-        Write-Output "Připojení úspěšně ověřeno."
+        Write-Output "Networking service running without any problem."
         Start-Sleep 30
         $test = 0
     }
     else{
-        Write-Output "Připojení nebylo úspěšně ověřeno."
         $test++
-        Write-Output "Restartuji WiFi adaptér, pokus $test ze 3."
+        Write-Output "Networking service restarted due to no ping response from 8.8.8.8, $test. attempt from 3."
         netsh interface set interface name="Wi-Fi" admin=DISABLED
         Start-Sleep 4
         netsh interface set interface name="Wi-Fi" admin=ENABLED
